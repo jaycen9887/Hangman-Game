87e7c41dded9c338c6d40e6a2dc42c5b623d4a05
Jaycen <jaycen9887@gmail.com>
2017-05-30 15:50:09 -0400

    fixing hint images links

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,7 +17,7 @@ var remain;
 
 
 var words = [["Darkwing Duck", "/assets/images/Darkwing.png"], ["Tiny Toons", "/assets/images/tinytoons.jpg"], ["Animaniacs", "/assets/images/animaniacs.png"], ["Rugrats", "/assets/images/rugrats.jpg"], ["Gargoyles", "/assets/images/gargoyles.jpg" ], ["Doug", "/assets/images/Doug.gif"], ["X-Men", "/assets/images/xmen.jpg"], ["Pinky and the Brain", "/assets/images/pinkyandthebrain.jpg"], ["Dexter's Laboratory", "/assets/images/dexterslab.jpg"], ["TaleSpin", "/assets/images/talespin.jpg"], ["Pokemon", "/assets/images/pokemon.jpg"], ["Earthworm Jim", "/assets/images/earthwormjim.jpg"], ["DuckTales", "/assets/images/ducktales.jpg"], ["Teenage Mutant Ninja Turtles", "/assets/images/tmnt.gif"], ["Garfield and Friends", "/assets/images/GarfieldandFriends.jpg"], ["Daria", "/assets/images/daria.jpg"], ["Hey Arnold!", "/assets/images/HeyArnold.jpg"], ["Johnny Bravo", "/assets/images/johnnybravo.png"], ["Goof Troop", "/assets/images/gooftroop.gif"], ["Captain Planet and the Planeteers", "/assets/images/captainplanetandtheplaneteers.jpg"], ["The Magic School Bus", "/assets/images/magicschoolbus.jpg"], ["Taz-Mania", "/assets/images/tazmania.jpg"], ["The Powerpuff Girls", "/assets/images/powerpuffgirls.png"], ["Beetlejuice", "/assets/images/beetlejuice.png"], ["Bobby's World", "/assets/images/Bobbysworld.png"], ["Spider-Man", "/assets/images/Spiderman.jpg"], ["Scooby Doo", "/assets/images/scoobydoo.jpg"], ["Street Sharks", "/assets/images/streetsharks.jpg"], ["Freakazoid!", "/assets/images/freakazoid.png"], ["Mighty Ducks", "/assets/images/mightyducks.jpg"], ["Chip 'n Dale", "/assets/images/chipanddale.jpg"], ["Rocko's Modern Life", "/assets/images/rockosmodernlife.jpg"], ["The Tick", "/assets/images/thetick.jpg"], ["Pepper Ann", "/assets/images/pepperann.jpg"], ["Courage the Cowardly Dog", "/assets/images/courage.jpg"], ["Inspector Gadget", "/assets/images/inspectorgadget.jpg"], ["Aaahh!!! Real Monsters", "/assets/images/aaahhrealmonsters.png"], ["Arthur", "/assets/images/arthur.jpg"], ["CatDog", "/assets/images/catdog.jpg"], ["Cow and Chicken", "/assets/images/CowandChicken.gif"], ["Recess", "/assets/images/recess.jpg"], ["ThunderCats", "/assets/images/thundercats.png"], ["Marsupilami", "/assets/images/marsupilami.png"], ["Ed, Edd, n Eddy", "/assets/images/ededdandeddy.jpg"], ["The Angry Beavers", 
-"/assets/images/angrybeavers.jpg"], ["The Smurfs", "/assets/images/smurfs.jpg"], ["The Ren & Stimpy Show", "/assets/images/renandstimpy.jpg"], ["Muppet Babies", "/assets/images/muppetbabies.jpg"], ["SpongeBob SquarePants", "/assets/images/spongebob.jpb"], ["Alvin and the Chipmunks", "/assets/images/alvinandthechipmunks.jpg"], ["Rocket Power", "/assets/images/rocketpower.jpeg"], ["The Wild Thornberrys", "/assets/images/wildthornberries.jpg"], ["The Flintstones", "/assets/images/flintsones.jpg"], ["The Jetsons", "/assets/images/jetsons.jpg"], ["Bugs Bunny", "/assets/images/bugsbunny.gif"], ["Looney Tunes", "/assets/images/looneytunes.jpg"], ["Biker Mice from Mars","/assets/images/bikermicefrommars.jpg"], ["Beavis and Butt-Head","/assets/images/beavisandbutthead.gif"]];
+"/assets/images/angrybeavers.jpg"], ["The Smurfs", "/assets/images/smurfs.jpg"], ["The Ren & Stimpy Show", "/assets/images/renandstimpy.jpg"], ["Muppet Babies", "/assets/images/muppetbabies.jpg"], ["SpongeBob SquarePants", "/assets/images/spongebob.jpg"], ["Alvin and the Chipmunks", "/assets/images/alvinandthechipmunks.jpg"], ["Rocket Power", "/assets/images/rocketpower.jpeg"], ["The Wild Thornberrys", "/assets/images/wildthornberries.jpg"], ["The Flintstones", "/assets/images/flintstones.jpg"], ["The Jetsons", "/assets/images/jetsons.jpg"], ["Bugs Bunny", "/assets/images/bugsbunny.gif"], ["Looney Tunes", "/assets/images/looneytunes.jpg"], ["Biker Mice from Mars","/assets/images/bikermicefrommars.jpg"], ["Beavis and Butt-Head","/assets/images/beavisandbutthead.gif"]];
 
 var randomWord = function() {
     var max = words.length;
@@ -52,7 +52,7 @@ var guessing = function(word , guess) {
     console.log(word);
     console.log(guess);
     if (word.includes(guess)) {
-        audio = new Audio("/assets/sounds/CorrectAnswer.wav");
+        audio = new Audio("/assets/sounds/Correct Answer.wav");
         audio.play();
     }else {
         audio = new Audio("/assets/sounds/wrongAnswer.wav");
@@ -81,15 +81,31 @@ var dashedWord = function(word) {
 
 
 var check = function(word, letter) {
-    for (var i = 0; i < word.length; i++) {
-        if (word.charAt(i) === letter) {
-            temp = dashed.slice(i, i);
-            console.log(temp);
-            correct++;
-        }
+    
+    /*Check if the word contains the letter*/
+    if(word.includes(letter)) {
         
+        for (var i = 0; i < word.length; i++) {
+            
+            /*if the word at char i = the letter */
+            if (word.charAt(i) === letter) {
+                
+                
+                
+                
+                
+
+ /******************** In dashed : remove the "_" in the spot of the letter and replace it with "letter"***********************************/ 
+
+                
+                
+                
+                
+                /* Increment guesses by 1 */
+                correct++;
+            }
+        }
     }
-    return dashed;
 };
 
 
@@ -105,7 +121,7 @@ document.onkeyup = function (event){
             if (!letters.includes(key)){
                 letters += (" " + key);
                 lettersGuessed.textContent = letters;
-                check(currentWord, key.toString());
+                check(currentWord, key);
                 guessing(currentWord, letters);
             }
 
@@ -115,7 +131,6 @@ document.onkeyup = function (event){
 
     }
     
-    wordPlaceHolder.textContent = dashed;
 };
 
 
@@ -127,12 +142,11 @@ console.log(currentWord);
 
 dashedWord(currentWord);
 
-
 winCount.textContent = wins;
 
 remain = letterCount - correct;
 
-
+wordPlaceHolder.textContent = dashed;
 
 
 
